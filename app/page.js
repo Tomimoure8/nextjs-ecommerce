abe65105c662e0bd73ea4887bb0782958c73e772
tomimoure8 <tomimoure8@gmail.com>
2025-01-17 13:56:48 -0300

    agregado de imagen PRUEBA UNO

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Image src={"/imageTea.png"} alt="UberElectronic" height={300} width={300}  />
     </>
   );
 }
