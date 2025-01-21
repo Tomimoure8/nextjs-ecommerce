ecd0ce39a5fd29aaea563ce1ef14fc50eeeb0d66
tomimoure8 <tomimoure8@gmail.com>
2025-01-21 18:08:59 -0300

    feat: agregar imagen en el header con enlace al inicio

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,20 @@
 import { ShoppingCart } from "lucide-react"
 import Link from "next/link"
+import Image from "next/image"
 
 function Header() {
     return (
         <header className="px-4 py-6 flex justify-between items-center bg-gradient-to-r from-green-400 via-purple-500 to-yellow-500 shadow-lg text-white">
             <h1 className="font-bold text-2xl tracking-wide">Comision 71810</h1>
             <nav className="flex gap-6">
+                <Link href="/">
+                    <Image
+                        src="/imagen-contacto_processed.jpg"
+                        alt="imagen-logo"
+                        width={100}
+                        height={100}
+                    />
+                </Link>
                 <Link href="/" className="hover:text-yellow-300 transition-colors duration-300 ease-in-out">Home</Link>
                 <Link href="/product" className="hover:text-yellow-300 transition-colors duration-300 ease-in-out">Productos</Link>
                 <Link href="/contact" className="hover:text-yellow-300 transition-colors duration-300 ease-in-out">Contacto</Link>
